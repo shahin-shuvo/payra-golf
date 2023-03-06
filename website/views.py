dd01e20ecc76b8aa182d57ccfe2086bda42a484b
shahin-shuvo <shahinshuvo96@gmail.com>
2023-03-07 01:04:14 +0600

    sheet added

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 import pymongo
 from pymongo import MongoClient
+import pandas as pd
+
 
 views = Blueprint('views', __name__)
 
@@ -11,17 +13,18 @@ dbs = client["pgc_db"]
 
 @views.route('/')
 def home():
-    table = dbs["executive_committee"]
-    exec_comte = table.find()
+    sheet_id = "1A2C7oX5pcR_M1b375sCQVJZ0eschowzVBOMkf6sIr-8"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    records = df.to_dict(orient="records")
 
     carousel_tbl = dbs["carousel_db"]
     carousel_img = carousel_tbl.find()
     
-    return render_template("home.html",myList = exec_comte, carousel_img = carousel_img)
+    return render_template("home.html",myList = records, carousel_img = carousel_img)
 
-@views.route('/aboutpgc')
-def aboutpgc():
-    return render_template("aboutpgc.html")
+@views.route('/aboutpgcc')
+def aboutpgcc():
+    return render_template("aboutpgcc.html")
 
 @views.route('/contact')
 def contact():
@@ -33,15 +36,35 @@ def gallery():
     gallery_img = gallery_table.find()
     return render_template("gallery.html" , gallery_img = gallery_img)
 
-@views.route('/exec_committee')
-def exec_committee():
-   
-    table = dbs["executive_committee"]
-    exec_comte = table.find()
-    return render_template("executive_committee.html", myList = exec_comte)
+
 
 @views.route('/notice')
 def notice():
     notice = dbs["notice_db"]
     notice_list = notice.find()
     return render_template("notice.html", noticeList = notice_list)
+
+
+@views.route('/member')
+def member():
+    # sheet_id = "1mXpaO4rbj34uFdUpwVjltaIEYzaQ2rxgVg75d7qG3mE"
+    # df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    # records = df.to_dict(orient="records")
+    return render_template("member.html", memberList = {})
+
+@views.route('/exec_committee')
+def exec_committee():
+    sheet_id = "1A2C7oX5pcR_M1b375sCQVJZ0eschowzVBOMkf6sIr-8"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    records = df.to_dict(orient="records")
+    return render_template("executive_committee.html", myList = records)
+
+
+
+@views.route('/tournament-summary')
+def tournament_summary():
+    sheet_id = "1PqVRcyl5Pp7RGXL-YbYOGwupG5jQEQACRokSjIl8dyw"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    records = df.to_dict(orient="records")
+    
+    return render_template("tournament_summary.html", myList = records)
