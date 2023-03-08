45b5af2bf35654f82122ab3fa75009c4dcfc77fd
shahin-shuvo <shahinshuvo96@gmail.com>
2023-03-09 01:13:44 +0600

    Update 9 Mar

--- website/views.py
+++ website/views.py
@@ -13,13 +13,16 @@ dbs = client["pgc_db"]
 
 @views.route('/')
 def home():
+    # For committe data
     sheet_id = "1A2C7oX5pcR_M1b375sCQVJZ0eschowzVBOMkf6sIr-8"
     df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
     records = df.to_dict(orient="records")
 
-    carousel_tbl = dbs["carousel_db"]
-    carousel_img = carousel_tbl.find()
-    
+    # For image link
+    sheet_id = "1BrbDXdeZTQqZOJ9NdiR_FFoyIxN2VbpxOGImm-N-tW8"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    carousel_img = df.to_dict(orient="records")
+    # print(carousel_img)
     return render_template("home.html",myList = records, carousel_img = carousel_img)
 
 @views.route('/aboutpgcc')
@@ -32,25 +35,44 @@ def contact():
 
 @views.route('/gallery')
 def gallery():
-    gallery_table = dbs["gallery_db"]
-    gallery_img = gallery_table.find()
-    return render_template("gallery.html" , gallery_img = gallery_img)
+    sheet_id="1lNBpItZy8wBb7eJvPg1uGTVaTayFfnx7QSTn5fYkCjI"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    records = df.to_dict(orient="records")
+    return render_template("gallery.html" , gallery_img = records)
 
 
 
 @views.route('/notice')
 def notice():
-    notice = dbs["notice_db"]
-    notice_list = notice.find()
-    return render_template("notice.html", noticeList = notice_list)
+    sheet_id = "1AQY2AjiYRo6R3LOkEj5Imp1CEeyDAMZT5Kovv_OQKKM"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    records = df.to_dict(orient="records")
+    return render_template("notice.html", noticeList = records)
 
 
-@views.route('/member')
-def member():
-    # sheet_id = "1mXpaO4rbj34uFdUpwVjltaIEYzaQ2rxgVg75d7qG3mE"
-    # df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
-    # records = df.to_dict(orient="records")
-    return render_template("member.html", memberList = {})
+@views.route('/membership')
+def membership():
+    sheet_id = "1mXpaO4rbj34uFdUpwVjltaIEYzaQ2rxgVg75d7qG3mE"
+    gid = "1654696814"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
+    records = df.to_dict(orient="records")
+    return render_template("member.html", memberList = records)
+
+@views.route('/subscription')
+def subscription():
+    sheet_id = "1mXpaO4rbj34uFdUpwVjltaIEYzaQ2rxgVg75d7qG3mE"
+    gid = "0"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
+    records = df.to_dict(orient="records")
+
+    gid = "1058097752"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
+    TempMemFee = df.to_dict(orient="records")
+
+    gid = "562291024"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
+    CorpMemFee = df.to_dict(orient="records")
+    return render_template("subscription.html", FullMemFee = records, TmpFee = TempMemFee, CorpFee = CorpMemFee)
 
 @views.route('/exec_committee')
 def exec_committee():
@@ -64,7 +86,24 @@ def exec_committee():
 @views.route('/tournament-summary')
 def tournament_summary():
     sheet_id = "1PqVRcyl5Pp7RGXL-YbYOGwupG5jQEQACRokSjIl8dyw"
+    sheet_name = "Sheet1"
     df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
     records = df.to_dict(orient="records")
     
     return render_template("tournament_summary.html", myList = records)
+
+
+@views.route('/swimming-pool')
+def swimming_pool():
+    sheet_id = "1C2hsqahx6XboezkkZPraI4JjCe45c2kLtJ9fMBN5yMk"
+    gid = "0"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}")
+    records = df.to_dict(orient="records")
+    print(records)
+    return render_template("swimmingpool.html", myList = records)
+
+
+@views.route('/guest-room')
+def guest_room():
+   
+    return render_template("guestroom.html")
